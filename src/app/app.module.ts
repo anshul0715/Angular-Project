02a59a4ebff1c8c38589ee7f1cdbbe62d0cf62ad
Anshul Agarwal <anshul.agarwal0715@gmail.com>
2021-02-06 16:39:52 +0530

    form added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home/home.component';
 import { Component1Component } from './component1/component1.component';
+import { HeaderComponent } from './header/header.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    Component1Component
+    Component1Component,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/component1/component1.component.ts
+++ src/app/component1/component1.component.ts
@@ -2,13 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-component1',
-  template: '<hr><h1>{{heading}}</h1><hr>',
+  templateUrl: './component1.component.html',
   styleUrls: ['./component1.component.scss']
 })
 export class Component1Component {
 
   constructor() { }
-
-  heading="this is component1";
 
 }
