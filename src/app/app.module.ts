a839854f0bf1294df07614408a4bfec49a14b192
Anshul Agarwal <anshul.agarwal0715@gmail.com>
2021-02-16 20:12:35 +0530

    routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -32,6 +33,7 @@ import { BrandComponent } from './brand/brand.component';
     AppRoutingModule,
     NgbModule,
     FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/brand/brand.component.ts
+++ src/app/brand/brand.component.ts
@@ -4,15 +4,13 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-brand',
   templateUrl: './brand.component.html',
  /**  template: `
-      <div style="min-height:100vh" fxLayout="column
+    <div style="min-height:100vh" fxLayout="column
       fxLayoutAlign="stretch>
-        
-
-<app-header></app-header>
-<router-outlet (activate)="onActivate()"></router-outlet>
-<span fxFlex="1 1 auto"></span>
-<app-footer></app-footer>
-      </div>
+      <app-header></app-header>
+      <router-outlet (activate)="onActivate()"></router-outlet>
+      <span fxFlex="1 1 auto"></span>
+      <app-footer></app-footer>
+    </div>
     `,
 */
   styleUrls: ['./brand.component.scss']
@@ -20,9 +18,9 @@ import { Component, OnInit } from '@angular/core';
 export class BrandComponent implements OnInit {
 
   constructor() { }
- onActivate(){
+ /**onActivate(){
    window.scroll(0,0);
- }
+ }*/
   ngOnInit(): void {
   }
 
