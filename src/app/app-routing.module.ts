602e8cf64bce59dbef92eba1f850254e3a56c6da
Anshul Agarwal <anshul.agarwal0715@gmail.com>
2021-03-02 13:30:36 +0530

    navbar update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,13 @@ import { BrandComponent } from './navbar-components/brand/brand.component';
 import { AuthGuard } from './auth.guard';
 import { LoginService } from './login.service';
 import { LoginComponent } from './login/login.component';
+import { signupComponent } from './home/signup/signup.component';
 const routes: Routes = [
   {path: 'brand',component:BrandComponent, canActivate:[AuthGuard]},
   {path: 'about',component:AboutComponent, canActivate:[AuthGuard]},
   {path: 'contact',component:ContactComponent, canActivate:[AuthGuard]},
   {path: "login", component :LoginComponent },
+  {path: "signup", component:signupComponent},
   {path: '**', redirectTo: 'login' }
 ];
 
